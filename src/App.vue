7da91cba97f0503315a4690ed1aa87a448e4d793
slp1608 <1025814831@qq.com>
2020-08-10 17:43:06 +0800

    Final version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HelloWorld />
   </div>
 </template>
 
@@ -17,12 +18,14 @@ export default {
 </script>
 
 <style>
+html,body{
+  padding: 0;
+  margin: 0;
+  /* background-color: #1e2327; */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,199 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-if="isHorizontalScreen">
+      <div class="main">
+        <div class="header">
+          <img src="../assets/bg.png" alt="">
+        </div>
+        <div class="subHeader">
+          <div class="line"></div>
+          <div class="subHeaderContent">
+            <div class="left"></div>
+            CHARACTER
+            <div class="right"></div>
+          </div>
+        </div>
+        <div class="content">
+          <div class="characterItem" v-for="item in imgList" :key="item.name">
+            <img :src="item.path" :alt="item.name">
+          </div>
+        </div>
+        <!-- <img class="pcImg" src="../assets/PC.jpg" alt="">
+        <img class="padImg" src="../assets/ipad.jpg" alt="">
+        <img class="moblieImg" src="../assets/../assets/moblie.png" alt=""> -->
+      </div>
+    </div>
+    <div v-else>
+      <div class="verticalScreenPage">
+        <img class="verticalScreenImg" alt="scape" src="../assets/scape.png">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data(){
+    return {
+      imgList: []
+    }
+  },
   props: {
-    msg: String
+    isHorizontalScreen: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  mounted() {
+    this.renderResize()
+    // 监听resize方法
+    window.addEventListener("resize", this.renderResize, false);
+    this.getData()
+  },
+   methods: {
+    renderResize() {
+      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      // 判断是否为手机端
+      if(flag){
+        let width = document.documentElement.clientWidth;
+        let height = document.documentElement.clientHeight;
+        if (width > height) {
+          this.isHorizontalScreen = true;
+          this.isVerticalScreen = false;
+        } else {
+          this.isHorizontalScreen = false;
+          this.isVerticalScreen = true;
+        }
+      } else {
+        this.isHorizontalScreen = true;
+      }
+    },
+    getData() {
+      // var xhr = new XMLHttpRequest();
+      // xhr.open('GET', url/file,true);
+      // xhr.onreadystatechange = function() {
+      //   if(xhr.readyState==4){
+      //         if(xhr.status==200){
+      //             var data=xhr.responseText;
+      //             console.log(data);
+      //   }
+      // };
+      // xhr.onerror = function() {
+      //   console.log("Oh, error");
+      // };
+      // xhr.send();
+      let that = this;
+      fetch("./demoData.json").then(response => response.json())
+      .then(function(data){
+          console.log(data)
+          that.imgList = data.data
+        })
+      .catch(err => console.log("Oh, error", err))
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.subHeader{
+  position: relative;
+  text-align: center;
+  color: #fff;
+  padding-top: 60px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.subHeader .line{
+  position: absolute;
+  top: 83px;
+  width: 100%;
+  height: 1px;
+  background-color: #111518;
 }
-li {
+.subHeaderContent{
+  position: relative;
+  z-index: 9;
   display: inline-block;
-  margin: 0 10px;
+  padding: 14px 20px;
+  border: 1px solid #30353b;
+  background-color: #111518;
 }
-a {
-  color: #42b983;
+.main{
+  background-color: #1e2327;
 }
+.main img{
+  width: 100%;
+}
+.content{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
+  padding: 60px 0;
+}
+/*moblie*/
+@media screen and (max-width:600px){
+  /* .moblieImg{
+    display: initial;
+  }
+  .pcImg, .padImg{
+    display: none;
+  } */
+  .characterItem{
+    width: 80%;
+  }
+}
+/*pad*/
+@media screen and (min-width:600px) and (max-width:960px){
+  /* .padImg{
+    display: initial;
+  }
+  .pcImg, .moblieImg{
+    display: none;
+  } */
+  .characterItem{
+    width: 40%;
+  }
+}
+/*PC*/
+@media screen and (min-width:960px){
+  /* .pcImg{
+    display: initial;
+  }
+  .moblieImg, .padImg{
+    display: none;
+  } */
+  .characterItem{
+    width: 30%;
+  }
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(-90deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+.verticalScreenPage{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  background-color: rgba(0,0,0,.5);
+}
+.verticalScreenImg{
+  width: 100%;
+  animation: spin 1s linear infinite;
+}
+.characterItem{
+    margin-bottom: 30px;
+  }
+
 </style>
